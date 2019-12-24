d9ceda11ffe26e2e2a170dc10f5f92e4cafd615d
Suraj K <48143140+suraj2498@users.noreply.github.com>
2019-12-23 21:41:05 -0500

    Updated EmployeesDB.java

--- EmployeesDatabaseProject/EmployeesDB.java
+++ EmployeesDatabaseProject/EmployeesDB.java
@@ -11,7 +11,7 @@ import java.sql.*;
 public class EmployeesDB {
     // FIELDS
     private static final String DB_NAME = "employeesDb.db"; // Name of database
-    private static final String CONNECTION_STRING = "jdbc:sqlite:C:\\Users\\srjkm\\Desktop\\" + DB_NAME;
+    private static final String CONNECTION_STRING = "jdbc:sqlite:/*Input Custom Path*/" + DB_NAME;
     ////////////////////////////////////EMPLOYEE QUERIES AND SQL STATEMENTS/////////////////////////////////////////////
     private static final String QUERY_EMPLOYEES_TABLE = "SELECT * FROM employees";
     private static final String ADD_EMPLOYEES_SQL = "INSERT INTO employees (socialSecurityNumber, firstName, lastName, birthday, " +
